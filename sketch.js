ac047cd3f4f53c859d000c60de1f0bdd569a1381
thanishkaa04 <thanishkaas07@gmail.com>
2021-06-22 16:48:06 -0700

    added platform

--- sketch.js
+++ sketch.js
@@ -34,7 +34,9 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100, 100);
+
+    platform = new Ground(150, 305, 300, 170);
 
 }
 
